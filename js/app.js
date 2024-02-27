f6896b8f030407a3b94c597156dd3444a3c75f0c
Haram Kwon <kharam1436@icloud.com>
2024-02-27 01:06:50 -0800

    protocol fixed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
+const protocol = window.location.protocol;
 const hostname = window.location.hostname;
 const port = window.location.port;
-const functionsURL = `http://${hostname}${!!port ? ':' + port : ''}/.netlify/functions`;
+const functionsURL = `${protocol}//${hostname}${!!port ? ':' + port : ''}/.netlify/functions`;
 
 const getRoute = async () => {
   const data = await fetch(`${functionsURL}/route`).then(response => response.json());
-  console.log(data);
+  console.log(JSON.stringify(data));
   return data;
 };
 

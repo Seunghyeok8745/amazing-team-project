cdc8f37374b3f750f18b2548b55b03056b0d7f7e
Haram Kwon <kharam1436@icloud.com>
2024-02-27 08:59:29 -0800

    mapkey demo addd

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const protocol = window.location.protocol;
 const hostname = window.location.hostname;
 const port = window.location.port;
-const functionsURL = `${protocol}//${hostname}${port ? `:${port}` : ''}/.netlify/functions`;
+const basesURL = `${protocol}//${hostname}${port ? `:${port}` : ''}/.netlify/functions`;
+const functionName = 'route';
 
 const getRoute = async () => {
-  const data = await fetch(`${functionsURL}/route`).then(response => response.json());
+  console.log(`BASE backend URL: ${basesURL}`);
+  const data = await fetch(`${basesURL}/${functionName}`).then(response => response.json());
   console.log(JSON.stringify(data));
   return data;
 };
--- netlify/functions/route.mjs
+++ netlify/functions/route.mjs
@@ -1,4 +1,5 @@
 export default async (req, context) => {
   const test = Netlify.env.get('test');
-  return new Response(JSON.stringify({ 'my test json': 'route.mjs', envTest: test }));
+  const map_key = Netlify.env.get('map_key');
+  return new Response(JSON.stringify({ 'my test json': 'route.mjs', envTest: test, map_key: map_key }));
 };

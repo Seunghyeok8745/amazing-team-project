52f6fea3ca91521ee5f429ced8e2ca7677443d01
Haram Kwon <kharam1436@icloud.com>
2024-02-27 01:02:11 -0800

    netlify deployment parsing error fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const hostname = window.location.hostname;
 const port = window.location.port;
-const functionsURL = `http://${hostname}:${port}/.netlify/functions`;
+const functionsURL = `http://${hostname}${!!port ? ':' + port : ''}/.netlify/functions`;
 
 const getRoute = async () => {
   const data = await fetch(`${functionsURL}/route`).then(response => response.json());

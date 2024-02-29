44b4571edb14d0e95270908336974f150b4998b5
Haram Kwon <kharam1436@icloud.com>
2024-02-29 01:24:50 -0800

    photo and location added

--- netlify/functions/route.mjs
+++ netlify/functions/route.mjs
@@ -1,5 +1,8 @@
 export default async (req, context) => {
+  const fullURL = req.url;
   const test = Netlify.env.get('test');
   const map_key = Netlify.env.get('map_key');
-  return new Response(JSON.stringify({ 'my test json': 'route.mjs', envTest: test, map_key: map_key, req, context }));
+  return new Response(
+    JSON.stringify({ 'my test json': 'route.mjs', envTest: test, map_key: map_key, req: JSON.stringify(req), context })
+  );
 };

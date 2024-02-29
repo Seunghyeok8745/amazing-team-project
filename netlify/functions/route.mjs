77cb81694e1c9578299c242447d1fb6e0415f586
Haram Kwon <kharam1436@icloud.com>
2024-02-29 00:25:17 -0800

    route testing

--- netlify/functions/route.mjs
+++ netlify/functions/route.mjs
@@ -1,5 +1,5 @@
 export default async (req, context) => {
   const test = Netlify.env.get('test');
   const map_key = Netlify.env.get('map_key');
-  return new Response(JSON.stringify({ 'my test json': 'route.mjs', envTest: test, map_key: map_key }));
+  return new Response(JSON.stringify({ 'my test json': 'route.mjs', envTest: test, map_key: map_key, req, context }));
 };

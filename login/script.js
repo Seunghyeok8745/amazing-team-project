1a50b4e1cbe35d3e9b54f836ced50a0c3d06e9aa
Haram Kwon <kharam1436@icloud.com>
2024-03-03 04:48:07 -0800

    from login page to main page fix

--- login/script.js
+++ login/script.js
@@ -1,14 +1,18 @@
-userId.addEventListener("focus", function (){userId.value = ""});
-password.addEventListener("focus", function (){password.value = ""});
+userId.addEventListener('focus', function () {
+  userId.value = '';
+});
+password.addEventListener('focus', function () {
+  password.value = '';
+});
 
 function login() {
-    const userId = document.getElementById("userId");
-    const password = document.getElementById("password");
+  const userId = document.getElementById('userId');
+  const password = document.getElementById('password');
 
-    if (userId.value === "" || password.value === "") {
-        alert("Please provide your login details to access your account.");
-        loginErrorMsg.style.opacity = 1;
-    } else {
-        window.location.href = 'http://localhost:8888/main/index.html';
-    }
+  if (userId.value === '' || password.value === '') {
+    alert('Please provide your login details to access your account.');
+    loginErrorMsg.style.opacity = 1;
+  } else {
+    window.location.href = '../main/index.html';
+  }
 }

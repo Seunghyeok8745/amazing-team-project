e23aec14bf17a24ae688ff28185120e4a7297d7e
Seunghyeok Kim <shkim8745@gmail.com>
2024-03-02 23:29:21 -0800

    page2 update

--- 4. main-page-UI/script.js
+++ 4. main-page-UI/script.js
@@ -1,58 +1,58 @@
 const swiper = new Swiper('.card_slider', {
-    direction: 'horizontal',
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    spaceBetween: 50,
-    loop: false,
-    speed: 1000,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  direction: 'horizontal',
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 50,
+  loop: false,
+  speed: 1000,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1200: {
-        slidesPerView: 4,
-      },
+    480: {
+      slidesPerView: 2,
     },
-  });
+    768: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
 
-  const imgBoxes = document.querySelectorAll('.img_box');
-  imgBoxes.forEach(function (box) {
-    box.addEventListener('mouseenter', function () {
-      this.querySelector('.slide-title').style.opacity = '0';
-      this.querySelector('.slide-subtitle').style.opacity = '0';
-      const overlayTexts = this.querySelectorAll('.overlay .slide-content *');
-      overlayTexts.forEach(function (text) {
-        text.style.color = 'white';
-      });
-    });
-    box.addEventListener('mouseleave', function () {
-      this.querySelector('.slide-title').style.opacity = '1';
-      this.querySelector('.slide-subtitle').style.opacity = '1';
+const imgBoxes = document.querySelectorAll('.img_box');
+imgBoxes.forEach(function (box) {
+  box.addEventListener('mouseenter', function () {
+    this.querySelector('.slide-title').style.opacity = '0';
+    this.querySelector('.slide-subtitle').style.opacity = '0';
+    const overlayTexts = this.querySelectorAll('.overlay .slide-content *');
+    overlayTexts.forEach(function (text) {
+      text.style.color = 'white';
     });
   });
-
-  // 페이지네이션 업데이트 함수
-  function updatePagination() {
-    const totalSlides = swiper.slides.length;
-    const totalPages = Math.ceil(totalSlides / 4);
-    const pagination = document.querySelector('.swiper-pagination');
-    const bullets = pagination.querySelectorAll('.swiper-pagination-bullet');
-  }
-
-  // 슬라이드 변경 시 페이지네이션 업데이트
-  swiper.on('slideChange', function () {
-    updatePagination();
+  box.addEventListener('mouseleave', function () {
+    this.querySelector('.slide-title').style.opacity = '1';
+    this.querySelector('.slide-subtitle').style.opacity = '1';
   });
+});
 
-  // 초기 페이지네이션 설정
+// 페이지네이션 업데이트 함수
+function updatePagination() {
+  const totalSlides = swiper.slides.length;
+  const totalPages = Math.ceil(totalSlides / 4);
+  const pagination = document.querySelector('.swiper-pagination');
+  const bullets = pagination.querySelectorAll('.swiper-pagination-bullet');
+}
+
+// 슬라이드 변경 시 페이지네이션 업데이트
+swiper.on('slideChange', function () {
   updatePagination();
+});
+
+// 초기 페이지네이션 설정
+updatePagination();
--- 4. main-page-UI/showList.js
+++ 4. main-page-UI/showList.js
@@ -1,26 +1,35 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, onAuthStateChanged ,signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore ,addDoc,collection,getDocs,doc,deleteDoc,updateDoc,query,where} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+  query,
+  where,
+} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP01jRV0UHrVPtMU9mC7wBsxd5J1uDs7U",
-  authDomain: "amazingteam-68fce.firebaseapp.com",
-  projectId: "amazingteam-68fce",
-  storageBucket: "amazingteam-68fce.appspot.com",
-  messagingSenderId: "963843107117",
-  appId: "1:963843107117:web:e3cbb5ae05e14c7adf8ba4",
-  measurementId: "G-T9G82HFNV4"
+  apiKey: 'AIzaSyCP01jRV0UHrVPtMU9mC7wBsxd5J1uDs7U',
+  authDomain: 'amazingteam-68fce.firebaseapp.com',
+  projectId: 'amazingteam-68fce',
+  storageBucket: 'amazingteam-68fce.appspot.com',
+  messagingSenderId: '963843107117',
+  appId: '1:963843107117:web:e3cbb5ae05e14c7adf8ba4',
+  measurementId: 'G-T9G82HFNV4',
 };
 
- const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
- const db = getFirestore(app)
+const db = getFirestore(app);
 
 async function showHistoryV() {
   try {
-  
-    const quertSnapshot = await getDocs(collection(db, "historyV"));
+    const quertSnapshot = await getDocs(collection(db, 'historyV'));
     const visitedHTMLArray = quertSnapshot.docs.map(doc => {
       const data = doc.data();
       // console.log(data,'data')
@@ -45,35 +54,32 @@ async function showHistoryV() {
        
       `;
     });
-    const visitedHTML = visitedHTMLArray .join(""); 
-    document.getElementById("showVisitedDiv").innerHTML = visitedHTML;
+    const visitedHTML = visitedHTMLArray.join('');
+    document.getElementById('showVisitedDiv').innerHTML = visitedHTML;
   } catch (error) {
     console.log(error, 'getHistory');
   }
 }
-showHistoryV()
+showHistoryV();
 showHistoryT();
 
+window.deleteBtnV = async function (id) {
+  try {
+    await deleteDoc(doc(db, 'historyV', id));
 
-window.deleteBtnV=async function(id){
-  try{
- await deleteDoc(doc(db,"historyV",id))
-
- await showHistoryV()
-  }catch(error){
-   console.log(error,'delete')
+    await showHistoryV();
+  } catch (error) {
+    console.log(error, 'delete');
   }
- }
+};
 
-
- //firestore 에서 gpt 가 준 명소 리스트 보여주기 함수
+//firestore 에서 gpt 가 준 명소 리스트 보여주기 함수
 async function showHistoryT() {
   try {
-  
-    const quertSnapshot = await getDocs(collection(db, "historyT"));
+    const quertSnapshot = await getDocs(collection(db, 'historyT'));
     const historyHTMLArray = quertSnapshot.docs.map(doc => {
       const data = doc.data();
-     
+
       return `
     
 
@@ -92,16 +98,15 @@ async function showHistoryT() {
 
       `;
     });
-    const historyHTML = historyHTMLArray.join(""); 
-     document.getElementById("recom").innerHTML = historyHTML;
-
+    const historyHTML = historyHTMLArray.join('');
+    document.getElementById('recom').innerHTML = historyHTML;
   } catch (error) {
     console.log(error, 'getHistory');
   }
 }
 //================================
 //*** showHistory() 는 버튼 위에잇어야합니다 ***/
-showHistoryT()  
+showHistoryT();
 //pin 버튼(위도경도 보여준다
 // window.pinBtn = async function(lng, lat) {
 //   try {
@@ -117,33 +122,29 @@ showHistoryT()
 // }
 //--------------------------------------
 //forestore 에 gpt 가 보여준것들 리스트 지워주는 버튼
-window.deleteBtnT=async function(id){
- try{
-await deleteDoc(doc(db,"historyT",id))
-await showHistoryT()
- }catch(error){
-  console.log(error,'delete')
- }
-}
+window.deleteBtnT = async function (id) {
+  try {
+    await deleteDoc(doc(db, 'historyT', id));
+    await showHistoryT();
+  } catch (error) {
+    console.log(error, 'delete');
+  }
+};
 
-
-window.visitedBtn=async function(name,address,description,email){
-  console.log(name,'name')
-  console.log(address,'address')
-  console.log(description,'description')
-  console.log(email,'email')
-try{
-  console.log(address,'email')
-  await addDoc(collection(db,"historyV"),{
-    address:address,
-    description:description,
-    name:name,
-   userEmail:email
-  });
-}catch(error){
-  console.log(error,'visitedBtn')
-}
-}
- 
-
- 
+window.visitedBtn = async function (name, address, description, email) {
+  console.log(name, 'name');
+  console.log(address, 'address');
+  console.log(description, 'description');
+  console.log(email, 'email');
+  try {
+    console.log(address, 'email');
+    await addDoc(collection(db, 'historyV'), {
+      address: address,
+      description: description,
+      name: name,
+      userEmail: email,
+    });
+  } catch (error) {
+    console.log(error, 'visitedBtn');
+  }
+};
